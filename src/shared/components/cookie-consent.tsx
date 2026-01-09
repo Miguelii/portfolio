5eef8e805f52323bdd4400a732dcbad39e194e8b
Miguel Gonçalves <miguelgoncalves18@hotmail.com>
2026-01-09 18:28:01 +0000

    fix: cookie consent

--- src/shared/components/cookie-consent.tsx
+++ src/shared/components/cookie-consent.tsx
@@ -23,6 +23,7 @@ export function CookieConsent() {
     const handler = (allow: boolean) => {
         startTransition(async () => {
             setIsOpen(false)
+            await createCookieConsentAction({ allowAnalytics: allow })
 
             if (allow) {
                 window.gtag('consent', 'update', {
@@ -31,11 +32,9 @@ export function CookieConsent() {
                 })
             }
 
+            router.refresh()
             setIsOpen(false)
             setHide(true)
-
-            await createCookieConsentAction({ allowAnalytics: allow })
-            router.refresh()
         })
     }
 
