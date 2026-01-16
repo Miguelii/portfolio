3b52902d18bbe5a8a7a228627f57ad07a54d33ff
Miguel Gonçalves <miguelgoncalves18@hotmail.com>
2026-01-16 10:53:55 +0000

    feat: timeframe

--- src/data/experience-data.ts
+++ src/data/experience-data.ts
@@ -21,13 +21,13 @@ export const ExperiencesData: WorkExperience[] = [
     },
     {
         company: 'CGI - Portugal',
-        totalTime: 'Sep 2022 - Set 2025 · 3 yrs 2 mos',
+        totalTime: 'Sep 2022 - Set 2025 · 3 yrs 1 mos',
         logoUrl: '/assets/cgi.webp',
         previewUrl: 'https://www.cgi.com/portugal/pt-pt',
         positions: [
             {
                 jobTitle: 'Software Engineer | Full-Stack',
-                timeLabel: 'Sep 2023 - Set 2025 · 2 yrs 8 mos',
+                timeLabel: 'May 2023 - Set 2025 · 2 yrs 5 mos',
                 achievements: [
                     'Lead Developer for the Web Team, specializing in building client portals and public websites.',
                     'Delivered products that have reached over million users worldwide, primarily using <b>React.js, Next.js and TypeScript.</b>',
@@ -38,7 +38,7 @@ export const ExperiencesData: WorkExperience[] = [
             },
             {
                 jobTitle: 'Junior Software Engineer | Full-Stack',
-                timeLabel: 'Sep 2022 - Fev 2023 · 6 mos',
+                timeLabel: 'Sep 2022 - May 2023 · 9 mos',
             },
         ],
     },
