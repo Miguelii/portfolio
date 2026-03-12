c2782d45b7bd66f9dba2e6ad3cc6c974e308496d
Miguel Gonçalves <miguelgoncalves18@hotmail.com>
2026-03-12 10:47:09 +0000

    feat: server env

--- src/env/server.ts
+++ src/env/server.ts
@@ -1,6 +1,12 @@
 import { createEnv } from '@t3-oss/env-nextjs'
+import * as z from 'zod/mini'
 
 export const ServerEnv = createEnv({
-    server: {},
-    experimental__runtimeEnv: process.env,
+    server: {
+        NODE_ENV: z.enum(['development', 'production', 'test']),
+    },
+    runtimeEnv: {
+        // NODE_ENV is automatically set by Node.js runtime
+        NODE_ENV: process.env.NODE_ENV,
+    },
 })
